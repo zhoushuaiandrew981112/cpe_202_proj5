975d34f9256528d389fe67c9aefd2bcbf04d9da5
Zhoushuai Wu <zwu17@unix1.csc.calpoly.edu>
2018-09-06 19:10:05 -0700

    sad...

--- graphwit.py
+++ graphwit.py
@@ -6,15 +6,37 @@
 
 #from queue import *
 import queue
+import re
 
 #adj_list = {"v0" : [("v1", 1), ("v2", 2), ("v3", 3), ...], "v2" : [("v1", 1), ("v2", 2), ...], ...}
 
 
-def get_paterns():
-#    result = []
+def get_patterns():
+    regex = r"[^A-Z]*([A-Z])[A-Z]*[^A-Z\d]*(\d*)(<?(?:-{1,2}|={1,2})>?)?(\d*)[^A-Z\d]*[A-Z]*([A-Z])[^A-Z]*"
+    r = re.compile(regex, re.I)
+    return [r]
 
 
-
+def parse_edges(patterns, strings, i_2 = 2, i_3 = 3, i_4 = 4):
+    adj_lst = {}
+    for s in strings:
+        m = re.search(patterns[0], s)
+        if m != None:
+            g_lst = m.groups()
+            if g_lst[0] != None and g_lst[i_4] != None and \
+                g_lst[i_2] != None and g_lst[i_2] != None:
+                a, b, l, r = g_lst[0], g_lst[i_4], g_lst[i_2][0], g_lst[i_2][-1]
+                wba = 1 if g_lst[1] == "" else int(g_lst[1])
+                wab = 1 if g_lst[i_3] == "" else int(g_lst[i_3])
+                if a != "" or b != "":
+                    if a not in adj_lst: adj_lst[a] = [] 
+                    if b not in adj_lst: adj_lst[b] = []
+                    if l != "<" and r == ">" : adj_lst[a].append((b, wab)) 
+                    elif l == "<" and r != ">" : adj_lst[b].append((a, wba)) 
+                    else:
+                        adj_lst[a].append((b, wab)) 
+                        adj_lst[b].append((a, wba))
+    return adj_lst
 
 
 def order_bfs(adj_list, start_vertex):
@@ -46,3 +68,10 @@ def order_dfs(adj_list, start_vertex):
                 q.put(tup[0])
                 break
     return result
+
+
+
+#r = get_patterns()
+#m = re.search(r[0], "!@#aAaAxyz+*3<-->/!qwertyb{zzZZ}&&")
+#lst = m.groups()
+#print(lst)
--- tests.py
+++ tests.py
@@ -26,6 +26,31 @@ class TestGraphWit(unittest.TestCase):
         self.assertEqual(order_bfs(adj_list, "d"), ["d", "a", "b", "c"])
         self.assertEqual(order_dfs(adj_list, "d"), ["d", "a", "b", "c"])
 
+    def test_get_pattern(self):
+        
+        self.assertEqual(get_patterns(), get_patterns())
+        self.assertEqual(get_patterns(), get_patterns())
+        self.assertEqual(get_patterns(), get_patterns())
+        self.assertEqual(get_patterns(), get_patterns())
+        self.assertEqual(get_patterns(), get_patterns())
+
+    def test_parse_edges(self):
+        patterns = get_patterns()
+        strings = ["!@#aAaAxyz+*3<-->/!qwertyb{zzZZ}&&"]
+        adj_lst = parse_edges(patterns, strings)
+        adj_lst = parse_edges(patterns, strings)
+        adj_lst = parse_edges(patterns, strings)
+        adj_lst = parse_edges(patterns, strings)
+        adj_lst = parse_edges(patterns, strings)
+        exp_lst = {"a" : [("b", 1)],"b" : [("a", 3)]}
+        self.assertEqual(adj_lst, exp_lst)
+
+        strings = ["!@#aAaAxyz+*3<-->/!qwertyb{zzZZ}&&"]
+        adj_lst = parse_edges(patterns, strings)
+        exp_lst = {"a" : [("b", 1)],"b" : [("a", 3)]}
+        self.assertEqual(adj_lst, exp_lst)
+
+
 
 if __name__ == '__main__':
     unittest.main()
